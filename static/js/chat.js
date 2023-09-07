223e644ab5ced819e9f06c4beac9e0b8afa7a496
joyjuke <joyjisunlee0123@gmail.com>
2023-09-07 17:14:17 +0900

    Git에게 모든 텍스트 파일에서 개행 문자를 자동으로 정규화하도록하는 파일

---
+++ static/js/chat.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    const chatBox = document.getElementById('chat-box');
+    const chatInput = document.getElementById('message');
+    const chatMessages = document.getElementById('messages');
+    const onlineCount = document.getElementById('users-online');
+
+    socket.on('message', function (data) {
+        const messageElem = document.createElement('div');
+
+        if (data.type && data.type === 'System') {
+            messageElem.classList.add('system-message'); // 시스템 메시지인 경우 'system-message' 클래스 추가
+        }
+        
+        // System: 이라는 문구 제거 로직
+        if (data.nickname) {
+            messageElem.textContent = data.nickname + ": " + data.message;
+        } else {
+            messageElem.textContent = data.message;
+        }
+
+        chatMessages.appendChild(messageElem);
+    
+        // 스크롤을 메시지 영역의 최하단으로 이동
+        chatBox.scrollTop = chatBox.scrollHeight;
+    });
+
+    socket.on('update_online_count', function (count) {
+        onlineCount.textContent = "현재 접속자 수: " + count + "명";
+    });
+
+    document.getElementById('sendBtn').addEventListener('click', function () {
+        const message = chatInput.value.trim();
+        if (message) {
+            socket.emit('message', { nickname: userNickname, message: message });
+            chatInput.value = '';
+        }
+    });
+
+
+    let userNickname = null;
+    document.getElementById('startChat').addEventListener('click', function () {
+        const nickname = document.getElementById('nickname-entry').value.trim();
+
+        if (nickname) {
+
+            userNickname = nickname;  // 서버에 전달할 닉네임 저장
+
+            // Hide entry box and show chat box
+            document.getElementById('entry-box').style.display = 'none';
+            document.getElementById('chat-box').style.display = 'block';
+
+            // Show chat input
+            document.querySelector('.chat-input').style.display = 'flex';
+
+            socket.emit('join', { nickname: nickname });
+        }
+    });
+
+    document.getElementById('sendBtn').addEventListener('click', function () {
+        const message = chatInput.value.trim();
+        if (message) {
+            socket.emit('message', { nickname: userNickname, message: message });
+            chatInput.value = '';
+        }
+        chatInput.focus();
+    });
+    
+    // Enter 키를 눌렀을 때 메시지 전송
+    chatInput.addEventListener('keydown', function (event) {
+        if (event.keyCode === 13 && !event.shiftKey) {
+            event.preventDefault();
+            document.getElementById('sendBtn').click();
+        }
+    });
+
+    window.addEventListener('beforeunload', function() {
+        if (userNickname) {
+            socket.emit('leave', { nickname: userNickname });
+        }
+    });
+});
+
+
---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+// const tl = gsap.timeline({
+// 	scrollTrigger: {
+// 		trigger: "#hero",
+// 		start: "top top",
+// 		end: "bottom top",
+// 		scrub: true
+// 	}
+// });
+
+// gsap.utils.toArray(".parallax").forEach(layer => {
+// 	const depth = layer.dataset.depth;
+// 	const movement = -(layer.offsetHeight * depth)
+// 	tl.to(layer, {y: movement, ease: "none"}, 0)
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+	let nav = document.getElementById('bottom-nav');
+	let home = document.getElementById('home');
+
+	let timeline1 = new TimelineMax();
+	timeline1
+		.to('.layers', 1, { y: 360, ease: Power3.easeInOut })
+		.from('.bottom-nav', 2, { width: '0%', autoAlpha: 0, ease: Power3.easeInOut }, '-=1.5')
+		.from('.box', 0.5, { y: 800, autoAlpha: 0, ease: Power3.easeInOut }, '-=0.5')
+		.from('nav', 2, { y: 0, autoAlpha: 0, scale: 1.8, ease: Power3.easeInOut }, '-=0.5');
+});
