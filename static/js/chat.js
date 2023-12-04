ccf36bd1b38c79885955b259b1308d4950fc84bf
joyjuke <joyjisunlee0123@gmail.com>
2023-12-04 15:26:56 +0900

    소켓 연결 에러 처리

--- static/js/chat.js
+++ static/js/chat.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (data.type && data.type === 'System') {
             messageElem.classList.add('system-message'); // 시스템 메시지인 경우 'system-message' 클래스 추가
         }
-        
+
         // System: 이라는 문구 제거 로직
         if (data.nickname) {
             messageElem.textContent = data.nickname + ": " + data.message;
@@ -21,14 +21,51 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         chatMessages.appendChild(messageElem);
-    
+
         // 스크롤을 메시지 영역의 최하단으로 이동
         chatBox.scrollTop = chatBox.scrollHeight;
     });
 
     socket.on('update_online_count', function (count) {
-        onlineCount.textContent = "현재 접속자 수: " + count + "명";
+        // onlineCount.textContent = "현재 접속자 수: " + count + "명";
+        onlineCount.textContent = "현재 접속자 수: " + 0 + "명";
     });
+
+    // 소켓 연결에 대한 에러 처리
+    socket.on('connect_error', (error) => {
+        console.error('Connection Error:', error);
+        const errorMessage = document.createElement('div');
+        errorMessage.classList.add('system-message'); // 시스템 메시지 스타일 적용
+        errorMessage.textContent = "서버와의 연결이 끊어졌습니다.";
+        chatMessages.appendChild(errorMessage);
+    });
+
+    // 소켓 연결 해제 시 처리
+    socket.on('disconnect', (reason) => {
+        console.warn('Disconnected:', reason);
+        const disconnectMessage = document.createElement('div');
+        disconnectMessage.classList.add('system-message');
+        disconnectMessage.textContent = "서버와의 연결이 끊어졌습니다.";
+        chatMessages.appendChild(disconnectMessage);
+
+        // 채팅 입력창 비활성화
+        chatInput.disabled = true;
+        document.getElementById('sendBtn').disabled = true;
+    });
+
+    // 소켓 연결 성공 시 처리
+    socket.on('connect', () => {
+        console.log('Connected to server');
+        const disconnectMessage = document.createElement('div');
+        disconnectMessage.classList.add('system-message');
+        disconnectMessage.textContent = "서버와 연결되었습니다.";
+        chatMessages.appendChild(disconnectMessage);
+
+        // 채팅 입력창 활성화
+        chatInput.disabled = false;
+        document.getElementById('sendBtn').disabled = false;
+    });
+
 
     let userNickname = null;
     document.getElementById('startChat').addEventListener('click', function () {
@@ -58,7 +95,7 @@ document.addEventListener("DOMContentLoaded", function () {
             chatInput.focus();  // 메시지 전송 후 다시 압력 필드에 포커스 맞춤
         }
     });
-    
+
     // Enter 키를 눌렀을 때 메시지 전송
     chatInput.addEventListener('keydown', function (event) {
         if (event.keyCode === 13 && !event.shiftKey) {
